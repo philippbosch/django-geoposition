0e1039d64c31a5a005c54ba8a1dcb80e45b1fc1e
Philipp Bosch <hello@pb.io>
2011-04-03 21:13:23 +0200

    Initial skeleton.

---
+++ README.md
@@ -0,0 +1,4 @@
+django-geoposition
+==================
+
+A model field that can hold a geoposition (latitude/longitude), and corresponding admin widget.
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-geoposition',
+    version=__import__('geoposition').__version__,
+    description='Django model field that can hold a geoposition, and corresponding admin widget.',
+    author='Philipp Bosch',
+    author_email='hello@pb.io',
+    url='http://github.com/philippbosch/django-geoposition',
+    packages=find_packages(),
+    zip_safe=False,
+    package_data = {
+        'robots': [
+            'locale/*/LC_MESSAGES/*',
+            'templates/geoposition/*.html',
+        ],
+    },
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Environment :: Web Environment',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: BSD License',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Framework :: Django',
+    ]
+)
