f14cf4cf6a730e52e037517063d1fec30fdf2b05
Philipp Bosch <hello@pb.io>
2011-04-04 15:44:16 +0200

    Extract Geoposition class from fields.py.

--- geoposition/__init__.py
+++ geoposition/__init__.py
@@ -1,2 +1,24 @@
+from decimal import Decimal
+
 VERSION = (0, 0, 1)
 __version__ = '.'.join(map(str, VERSION))
+
+
+class Geoposition(object):
+    def __init__(self, latitude, longitude):
+        if isinstance(latitude, float) or isinstance(latitude, int):
+            latitude = str(latitude)
+        if isinstance(longitude, float) or isinstance(longitude, int):
+            longitude = str(longitude)
+        
+        self.latitude = Decimal(latitude)
+        self.longitude = Decimal(longitude)
+    
+    def __unicode__(self):
+        return "%s,%s" % (self.latitude, self.longitude)
+    
+    def __repr__(self):
+        return "Geoposition(%s)" % unicode(self)
+    
+    def __len__(self):
+        return len(unicode(self))
--- geoposition/fields.py
+++ geoposition/fields.py
@@ -1,27 +1,7 @@
-from decimal import Decimal
 from django.db import models
 
-from forms import GeopositionField as GeopositionFormField
-
-
-class Geoposition(object):
-    def __init__(self, latitude, longitude):
-        if isinstance(latitude, float) or isinstance(latitude, int):
-            latitude = str(latitude)
-        if isinstance(longitude, float) or isinstance(longitude, int):
-            longitude = str(longitude)
-        
-        self.latitude = Decimal(latitude)
-        self.longitude = Decimal(longitude)
-    
-    def __unicode__(self):
-        return "%s,%s" % (self.latitude, self.longitude)
-    
-    def __repr__(self):
-        return "Geoposition(%s)" % unicode(self)
-    
-    def __len__(self):
-        return len(unicode(self))
+from . import Geoposition
+from .forms import GeopositionField as GeopositionFormField
 
 
 class GeopositionField(models.Field):
