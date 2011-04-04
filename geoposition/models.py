aceb0b0c84e013b19b570e79253a4f0b616ab0df
Philipp Bosch <hello@pb.io>
2011-04-04 15:45:03 +0200

    Remove south dependency.

--- geoposition/models.py
+++ geoposition/models.py
@@ -1,2 +1,5 @@
-from south.modelsinspector import add_introspection_rules
-add_introspection_rules([], ["^geoposition\.fields\.GeopositionField"])
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^geoposition\.fields\.GeopositionField"])
+except ImportError:
+    pass
