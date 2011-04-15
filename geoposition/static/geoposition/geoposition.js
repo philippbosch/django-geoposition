e82aa9b4c7fddada75d69e762b67403e4e69378a
Philipp Bosch <hello@pb.io>
2011-04-15 17:49:35 +0200

    Add address search.

--- geoposition/static/geoposition/geoposition.js
+++ geoposition/static/geoposition/geoposition.js
@@ -9,6 +9,8 @@
             var $container = $(this),
                 $mapContainer = $('<div class="geoposition-map" />'),
                 $addressRow = $('<div class="geoposition-address" />'),
+                $searchRow = $('<div class="geoposition-search" />'),
+                $searchInput = $('<input>', {'type': 'search', 'placeholder': 'Search …'}),
                 $latitudeField = $container.find('input.geoposition:eq(0)'),
                 $longitudeField = $container.find('input.geoposition:eq(1)'),
                 latitude = parseFloat($latitudeField.val()) || 0,
@@ -18,7 +20,51 @@
                 mapOptions,
                 marker;
             
-            $container.append($mapContainer, $addressRow);
+            
+            $searchInput.bind('keydown', function(e) {
+                if (e.keyCode == 13) {
+                    e.preventDefault();
+                    var $input = $(this),
+                        gc = new google.maps.Geocoder();
+                    $input.parent().find('ul.geoposition-results').remove();
+                    gc.geocode({
+                        'address': $(this).val()
+                    }, function(results, status) {
+                        if (status == 'OK') {
+                            var updatePosition = function(result) {
+                                if (result.geometry.bounds) {
+                                    map.fitBounds(result.geometry.bounds);
+                                } else {
+                                    map.panTo(result.geometry.location);
+                                    map.setZoom(18);
+                                }
+                                marker.setPosition(result.geometry.location);
+                                google.maps.event.trigger(marker, 'dragend');
+                            };
+                            
+                            if (results.length == 1) {
+                                updatePosition(results[0]);
+                            } else {
+                                var $ul = $('<ul />', {'class': 'geoposition-results'});
+                                $.each(results, function(i, result) {
+                                    var $li = $('<li />');
+                                    $li.text(result.formatted_address);
+                                    $li.bind('click', function() {
+                                        updatePosition(result);
+                                        $li.closest('ul').remove();
+                                    });
+                                    $li.appendTo($ul);
+                                });
+                                $input.after($ul);
+                            }
+                        }
+                    });
+                }
+            }).bind('abort', function() {
+                $(this).parent().find('ul.geoposition-results').remove();
+            });
+            $searchInput.appendTo($searchRow);
+            $container.append($mapContainer, $addressRow, $searchRow);
             
             mapLatLng = new google.maps.LatLng(latitude, longitude);
             mapOptions = $.extend({}, mapDefaults, {
