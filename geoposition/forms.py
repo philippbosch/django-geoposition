5b75098e9bfc2cfb1419398ca299006567525653
Carles Barrobés <carles@barrobes.com>
2013-07-05 16:45:59 +0100

    Ensure GeopositionField works in inline forms

--- geoposition/forms.py
+++ geoposition/forms.py
@@ -2,25 +2,30 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 from .widgets import GeopositionWidget
+from . import Geoposition
+
 
 class GeopositionField(forms.MultiValueField):
     default_error_messages = {
         'invalid': _('Enter a valid geoposition.')
     }
-    
+
     def __init__(self, *args, **kwargs):
         self.widget = GeopositionWidget()
         fields = (
             forms.DecimalField(label=_('latitude')),
             forms.DecimalField(label=_('longitude')),
         )
-        super(GeopositionField, self).__init__(fields, required=False)
-    
+        kwargs['required'] = False
+        if 'initial' in kwargs:
+            kwargs['initial'] = Geoposition(*kwargs['initial'].split(','))
+        super(GeopositionField, self).__init__(fields, **kwargs)
+
     def widget_attrs(self, widget):
         classes = widget.attrs.get('class', '').split()
         classes.append('geoposition')
         return {'class': ' '.join(classes)}
-    
+
     def compress(self, value_list):
         if value_list:
             return value_list
