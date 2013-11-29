7f28c2c5a51857afa5d6542a12e80c0c4dcdf88c
Håkan Waara <hwaara@gmail.com>
2013-11-29 14:19:20 +0100

    * Make the search widget show autosuggestions after a short delay, or immediately on enter * Move up the search widget to the top left so you don't miss it under the whole map

--- geoposition/static/geoposition/geoposition.js
+++ geoposition/static/geoposition/geoposition.js
@@ -16,7 +16,7 @@ if (jQuery != undefined) {
                 $mapContainer = $('<div class="geoposition-map" />'),
                 $addressRow = $('<div class="geoposition-address" />'),
                 $searchRow = $('<div class="geoposition-search" />'),
-                $searchInput = $('<input>', {'type': 'search', 'placeholder': 'Search …'}),
+                $searchInput = $('<input>', {'type': 'search', 'placeholder': 'Start typing an address …'}),
                 $latitudeField = $container.find('input.geoposition:eq(0)'),
                 $longitudeField = $container.find('input.geoposition:eq(1)'),
                 latitude = parseFloat($latitudeField.val()) || 0,
@@ -27,50 +27,65 @@ if (jQuery != undefined) {
                 marker;
 
 
+            function doSearch() {
+                var gc = new google.maps.Geocoder();
+                $searchInput.parent().find('ul.geoposition-results').remove();
+                gc.geocode({
+                    'address': $searchInput.val()
+                }, function(results, status) {
+                    if (status == 'OK') {
+                        var updatePosition = function(result) {
+                            if (result.geometry.bounds) {
+                                map.fitBounds(result.geometry.bounds);
+                            } else {
+                                map.panTo(result.geometry.location);
+                                map.setZoom(18);
+                            }
+                            marker.setPosition(result.geometry.location);
+                            google.maps.event.trigger(marker, 'dragend');
+                        };
+                        if (results.length == 1) {
+                            updatePosition(results[0]);
+                        } else {
+                            var $ul = $('<ul />', {'class': 'geoposition-results'});
+                            $.each(results, function(i, result) {
+                                var $li = $('<li />');
+                                $li.text(result.formatted_address);
+                                $li.bind('click', function() {
+                                    updatePosition(result);
+                                    $li.closest('ul').remove();
+                                });
+                                $li.appendTo($ul);
+                            });
+                            $searchInput.after($ul);
+                        }
+                    }
+                });
+            }
+
+            var autoSuggestTimer = null;
             $searchInput.bind('keydown', function(e) {
+                if (autoSuggestTimer) {
+                    clearTimeout(autoSuggestTimer);
+                    autoSuggestTimer = null;
+                }
+
+                // if enter, search immediately
                 if (e.keyCode == 13) {
                     e.preventDefault();
-                    var $input = $(this),
-                        gc = new google.maps.Geocoder();
-                    $input.parent().find('ul.geoposition-results').remove();
-                    gc.geocode({
-                        'address': $(this).val()
-                    }, function(results, status) {
-                        if (status == 'OK') {
-                            var updatePosition = function(result) {
-                                if (result.geometry.bounds) {
-                                    map.fitBounds(result.geometry.bounds);
-                                } else {
-                                    map.panTo(result.geometry.location);
-                                    map.setZoom(18);
-                                }
-                                marker.setPosition(result.geometry.location);
-                                google.maps.event.trigger(marker, 'dragend');
-                            };
-
-                            if (results.length == 1) {
-                                updatePosition(results[0]);
-                            } else {
-                                var $ul = $('<ul />', {'class': 'geoposition-results'});
-                                $.each(results, function(i, result) {
-                                    var $li = $('<li />');
-                                    $li.text(result.formatted_address);
-                                    $li.bind('click', function() {
-                                        updatePosition(result);
-                                        $li.closest('ul').remove();
-                                    });
-                                    $li.appendTo($ul);
-                                });
-                                $input.after($ul);
-                            }
-                        }
-                    });
+                    doSearch();
+                }
+                else {
+                    // otherwise, search after a while after typing ends
+                    autoSuggestTimer = setTimeout(function(){
+                        doSearch();
+                    }, 1000);
                 }
             }).bind('abort', function() {
                 $(this).parent().find('ul.geoposition-results').remove();
             });
             $searchInput.appendTo($searchRow);
-            $container.append($mapContainer, $addressRow, $searchRow);
+            $container.append($searchRow, $mapContainer, $addressRow);
 
             mapLatLng = new google.maps.LatLng(latitude, longitude);
             mapOptions = $.extend({}, mapDefaults, {
