0d6b06e5207fe9d55ce87fa62c4f6ed84794f369
Philipp Bosch <hello@pb.io>
2013-12-23 17:04:14 +0100

    Setup example app again.

--- geoposition/tests/settings.py
+++ geoposition/tests/settings.py
@@ -1,25 +1,12 @@
-"""
-Django settings for example project.
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '+*mac$-+vop%1#8zp%)blgacvst%fm)p$&u#p!5(x5nv9c0955'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -29,6 +16,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'geoposition',
+    'example',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -40,13 +28,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'example.urls'
-
-WSGI_APPLICATION = 'example.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+ROOT_URLCONF = 'geoposition.tests.urls'
 
 DATABASES = {
     'default': {
@@ -54,9 +36,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -67,9 +46,5 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
