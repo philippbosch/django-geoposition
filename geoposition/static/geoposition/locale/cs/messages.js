0a331ff708336a407b5000402768ac0d78dc0c37
Filip Novák <filip.f.novak@gmail.com>
2014-06-24 21:38:57 +0200

    Czech translation of js

---
+++ geoposition/static/geoposition/locale/cs/messages.js
@@ -0,0 +1,8 @@
+/*
+ * Czech translate of django-geoposition js file
+ * author: Filip Novak, filip.f.novak@gmail.com
+ */
+var geopositionMessages = {
+	'Start typing an address …': 'Začněte psát hledanou adresu...',
+	'geoposition: "google" not defined.  You might not be connected to the internet.': 'geoposition: objekt "google" není definován. Musíte být připojeni k internetu.'
+};
