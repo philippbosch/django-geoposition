fcbf989edb0580d111956b1ab6969c2cb939315f
Lance Rogers <lancekincaid1994@gmail.com>
2015-08-05 12:23:49 -0400

    try

--- geoposition/widgets.py
+++ geoposition/widgets.py
@@ -25,13 +25,7 @@ class GeopositionWidget(forms.MultiWidget):
 
     def format_output(self, rendered_widgets):
         return render_to_string('geoposition/widgets/geoposition.html', {
-            'latitude': {
-                'html': rendered_widgets[0],
-                'label': _("latitude"),
-            },
-            'longitude': {
-                'html': rendered_widgets[1],
-                'label': _("longitude"),
+        
             },
             'config': {
                 'map_widget_height': settings.GEOPOSITION_MAP_WIDGET_HEIGHT,
