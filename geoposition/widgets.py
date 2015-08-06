0046c708200836dc95776a0cf6f7aebdd1b82304
Lance Rogers <lancekincaid1994@gmail.com>
2015-08-06 01:26:08 -0400

    went backwards

--- geoposition/widgets.py
+++ geoposition/widgets.py
@@ -6,7 +6,6 @@ from django.template.loader import render_to_string
 from django.utils import six
 from django.utils.translation import ugettext_lazy as _
 from .conf import settings
-from .custom_map import Options
 
 class GeopositionWidget(forms.MultiWidget):
     def __init__(self, attrs=None):
@@ -91,47 +90,3 @@ class MapOnlyWidget(forms.MultiWidget):
             'all': ('geoposition/geoposition.css',)
         }
 
-
-class AdjustableMapOnlyWidget(forms.MultiWidget):
-    def __init__(self, attrs=None):
-        widgets = (
-            forms.TextInput(),
-            forms.TextInput(),
-            forms.IntegerField(),
-            forms.CharField(),
-            forms.CharField(),
-        )
-        super(AdjustableMapOnlyWidget, self).__init__(widgets, attrs)
-
-    def decompress(self, value):
-        if isinstance(value, six.text_type):
-            return value.rsplit(',')
-        if value:
-            return [value.latitude, value.longitude]
-        return [None,None]
-
-    def format_output(self, rendered_widgets):
-        return render_to_string('geoposition/widgets/adjustable-map-only.html', {
-            'latitude': {
-                'html': rendered_widgets[0],
-                'label': _("latitude"),
-            },
-            'longitude': {
-                'html': rendered_widgets[1],
-                'label': _("longitude"),
-            },
-            'config': {
-                'map_widget_height': rendered_widgets[2],
-                'map_options': rendered_widgets[3],
-                'marker_options': rendered_widgets[4],
-            }
-        })
-
-    class Media:
-        js = (
-            '//maps.google.com/maps/api/js?sensor=false',
-            'geoposition/geoposition.js',
-        )
-        css = {
-            'all': ('geoposition/geoposition.css',)
-        }
