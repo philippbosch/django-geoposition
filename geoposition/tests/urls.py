a53c7b23e62519b19c3a3a7176e51efdc0f441c5
ZodiacFireworks <martin.vuelta@gmail.com>
2016-05-14 13:18:02 -0500

    deleter unnecesary testing files

--- geoposition/tests/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-
-admin.autodiscover()
-
-urlpatterns = patterns('',
-    url(r'^$', 'example.views.poi_list'),
-    url(r'^admin/', include(admin.site.urls)),
-)
