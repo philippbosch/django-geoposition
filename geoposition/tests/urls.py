30b07b44277f9fe30dc5e753d1d4ab322feb6f56
Richard Cotrina <rcotrina94@users.noreply.github.com>
2017-05-15 13:23:03 -0500

    django.conf.urls.patterns is deprecated

--- geoposition/tests/urls.py
+++ geoposition/tests/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', 'example.views.poi_list'),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
