bba0d4493b7c738db349954aa1337c55c2a5f7a6
romamo <romamo@yandex.ru>
2018-03-16 20:20:32 +0200

    Updated to support Django 2.0

--- geoposition/widgets.py
+++ geoposition/widgets.py
@@ -9,12 +9,36 @@ from .conf import settings
 
 
 class GeopositionWidget(forms.MultiWidget):
+    template_name = 'geoposition/widgets/geoposition.html'
+
     def __init__(self, attrs=None):
         widgets = (
             forms.TextInput(),
             forms.TextInput(),
         )
         super(GeopositionWidget, self).__init__(widgets, attrs)
+        
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        if not isinstance(value, list):
+            value = self.decompress(value)
+
+        context['widget'] = {
+            'latitude': {
+                'html': value[0],
+                'label': _("latitude"),
+            },
+            'longitude': {
+                'html': value[1],
+                'label': _("longitude"),
+            },
+            'config': {
+                'map_widget_height': settings.MAP_WIDGET_HEIGHT or 500,
+                'map_options': json.dumps(settings.MAP_OPTIONS),
+                'marker_options': json.dumps(settings.MARKER_OPTIONS),
+            }
+        }
+        return context
 
     def decompress(self, value):
         if isinstance(value, six.text_type):
@@ -23,22 +47,6 @@ class GeopositionWidget(forms.MultiWidget):
             return [value.latitude, value.longitude]
         return [None, None]
 
-    def format_output(self, rendered_widgets):
-        return render_to_string('geoposition/widgets/geoposition.html', {
-            'latitude': {
-                'html': rendered_widgets[0],
-                'label': _("latitude"),
-            },
-            'longitude': {
-                'html': rendered_widgets[1],
-                'label': _("longitude"),
-            },
-            'config': {
-                'map_widget_height': settings.MAP_WIDGET_HEIGHT or 500,
-                'map_options': json.dumps(settings.MAP_OPTIONS),
-                'marker_options': json.dumps(settings.MARKER_OPTIONS),
-            }
-        })
 
     class Media:
         js = (
