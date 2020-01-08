3db8e5fd04cb589beb43119c191352727844e6f0
Rasmus Schlünsen <rasmus@schlunsen.com>
2020-01-08 10:52:00 +0100

    Fixed for Django 3

--- geoposition/widgets.py
+++ geoposition/widgets.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 import json
 from django import forms
 from django.template.loader import render_to_string
-from django.utils import six
+import six
 from django.utils.translation import ugettext_lazy as _
 from .conf import settings
 
